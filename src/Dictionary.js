0c608dddabda3d2ce7d01cf20fa593cf70b60c54
Ila-bura <ila.bura@gmail.com>
2021-06-06 10:00:50 +0100

    Added Api documentation reference

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -11,6 +11,8 @@ console.log(response.data[0]);
 
 function search (event) {
     event.preventDefault();
+
+    // documentation: https://api.dictionaryapi.dev/
     let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
 }
