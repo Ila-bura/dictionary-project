aefa498019f6f0d96667d96c20c4437fa2b3532d
Ila-bura <ila.bura@gmail.com>
2021-06-06 09:21:33 +0100

    Search engine implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Dictionary from "./Dictionary";
 import logo from "./logo.png";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo}
         className="App-logo img-fluid"
@@ -12,6 +14,14 @@ export default function App() {
       />
       
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>
+        Coded by Ila with SheCodes</small>
+      </footer>
+    </div>
     </div>
   );
 }
