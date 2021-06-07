d0636264c7fcd2cbdaf88b57c29413d5cad15374
Ila-bura <ila.bura@gmail.com>
2021-06-07 15:14:41 +0100

    Added images API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,13 +8,25 @@ let [keyword, setKeyword] = useState(props.defaultKeyword);
 let [results, setResults] = useState(null);
 let [loaded, setLoaded] = useState(false);
 
-function handleResponse(response) {
+function handleDictionaryResponse(response) {
 setResults(response.data[0]);
     }
+
+function handlePexelsResponse(response) {
+    console.log(response);
+
+}
+
+
 function search() {
     // documentation: https://api.dictionaryapi.dev/
     let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let pexelsApiKey = "563492ad6f91700001000001708d4b800ce54af2bf974cd47ac96199";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
+    axios.get(pexelsApiUrl, { headers: {"Authorization" : `Bearer ${pexelsApiKey}` },})
+    .then(handlePexelsResponse);
 }
 
 function handleSubmit (event) {
